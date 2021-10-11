30efc9a9bce70f3e549d892a930a87c334aa014f
pc <pc>
2021-10-11 21:40:19 +0200

    Add project files.

---
+++ TCP Server/BookRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookClassLibrary;
+
+namespace TCP_Server
+{
+    public class BookRepo
+    {
+        public static readonly List<Book> BookList = new List<Book>()
+        {
+            new Book(){Author = "Pedro", Isbn = "01010101", PageNumber = 800, Title = "Nice"},
+            new Book(){Author = "John", Isbn = "91919191", PageNumber = 800, Title = "IT Essentials"},
+            new Book(){Author = "Jane", Isbn = "30303003", PageNumber = 800, Title = "I have no imagination"}
+        };
+
+        public List<Book> GetAll()
+        {
+            List<Book> result = new List<Book>(BookList);
+            return result;
+        }
+        public Book Get(string isbn)
+        {
+            return BookList.Find(book => book.Isbn == isbn);
+        }
+        public Book Add(Book newBook)
+        {
+            BookList.Add(newBook);
+            return newBook;
+        }
+    }
+}
---
+++ TCP Server/BookTCPServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using BookClassLibrary;
+
+namespace TCP_Server
+{
+    class BookTcpServer
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 4646);
+            listener.Start();
+            Console.WriteLine($"Server has started {IPAddress.Loopback}:4646");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(() => { HandleClient(socket); });
+            }
+        }
+        public static void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            BookRepo repo = new BookRepo();
+            Console.WriteLine("Client connected");
+
+            while (true)
+            {
+                string command = reader.ReadLine();
+                string parameter = reader.ReadLine();
+
+                if (command == "GetAll" && string.IsNullOrEmpty(parameter))
+                {
+                    var Books = repo.GetAll();
+
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+                    string jsonString = JsonSerializer.Serialize(Books, options);
+
+
+                    writer.WriteLine(jsonString);
+                    writer.Flush();
+                }
+                else if (command == "Get" && BookRepo.BookList.Any(b => b.Isbn == parameter))
+                {
+                    Book foundBook = repo.Get(parameter);
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+                    string jsonString = JsonSerializer.Serialize(foundBook, options);
+                }
+
+
+
+                writer.Flush();
+            }
+        }
+    }
+}
